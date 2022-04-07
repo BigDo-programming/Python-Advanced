354d8cbd5740900862b373cb92350d5cf734daa1
Andon Andonov <bloodtaint@yahoo.com>
2022-04-07 21:55:43 +0300

    07.04.22/2

--- 02_lists_as_stacks_and_queues_exercise/truck_tour.py
+++ 02_lists_as_stacks_and_queues_exercise/truck_tour.py
@@ -1,10 +1,47 @@
 import sys
 from io import StringIO
 
-input1 = """"""
-input2 = """"""
+input1 = """3
+1 5
+10 3
+3 4"""
+input2 = """5
+22 5
+14 10
+52 7
+21 12
+36 9"""
 input3 = """"""
 
-# sys.stdin = StringIO(input1)
-sys.stdin = StringIO(input2)
+sys.stdin = StringIO(input1)
+# sys.stdin = StringIO(input2)
 # sys.stdin = StringIO(input3)
+
+
+from collections import deque
+
+queue = deque()
+n = int(input())
+
+
+for i in range(n):
+    pump = [int(x) for x in input().split()]
+    queue.append(pump)
+
+for index in range(n):
+    car_fuel = 0
+    completed = True
+    for petrol, distance in queue:
+
+        car_fuel += petrol
+        if distance > car_fuel:
+            completed = False
+            break
+        car_fuel -= distance
+    if completed:
+
+        break
+
+    queue.append(queue.popleft())
+
+print(index)
