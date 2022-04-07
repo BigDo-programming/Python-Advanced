bfe3d8ae74cc4f4689b52aa6407da6a45cde7b5e
Andon Andonov <bloodtaint@yahoo.com>
2022-04-07 21:56:56 +0300

    07.04.22/3

--- 02_lists_as_stacks_and_queues_exercise/robotics.py
+++ 02_lists_as_stacks_and_queues_exercise/robotics.py
@@ -1,41 +1,10 @@
 import sys
 from io import StringIO
 
-input1 = """{[()]}"""
-input2 = """{[(])}"""
+input1 = """"""
+input2 = """"""
 input3 = """"""
 
 # sys.stdin = StringIO(input1)
 sys.stdin = StringIO(input2)
 # sys.stdin = StringIO(input3)
-
-balanced = True
-parentheses = input()
-stack = []
-for parent in parentheses:
-    if parent in "{[(":
-        stack.append(parent)
-
-    elif parent in "}])":
-        if len(stack) == 0:
-            balanced = False
-            break
-
-        opening_paren = stack.pop()
-
-        if f'{opening_paren}{parent}' not in ["[]", "{}", "()"]:
-            balanced = False
-            break
-
-        # if parent == "]" and opening_paren == "[":
-        #     continue
-        # elif parent == "}" and opening_paren == "{":
-        #     continue
-        # elif parent == ")" and opening_paren == "(":
-        #     continue
-
-
-if balanced and len(stack) == 0:
-    print("YES")
-else:
-    print("NO")
