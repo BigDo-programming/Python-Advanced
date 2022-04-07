d095e545e18b3ad9b04b2a7eca516de39b46e036
Andon Andonov <bloodtaint@yahoo.com>
2022-04-07 10:30:12 +0300

    07.04.22/1

---
+++ 02_lists_as_stacks_and_queues_exercise/key_revolver.py
@@ -0,0 +1,10 @@
+import sys
+from io import StringIO
+
+input1 = """"""
+input2 = """"""
+input3 = """"""
+
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
+# sys.stdin = StringIO(input3)
