7d829a2ef027b15822914022bbc6d1deda54b783
Andon Andonov <bloodtaint@yahoo.com>
2022-04-18 19:21:46 +0300

    18.04.22/2

--- 08_multidimensional_lists_exercise_2/flatten_lists.py
+++ 08_multidimensional_lists_exercise_2/flatten_lists.py
@@ -1,6 +1,12 @@
 import sys
 from io import StringIO
 
-input1 = """"""
+input1 = """1 2 3 |4 5 6 |  7  88"""
 
 sys.stdin = StringIO(input1)
+
+sublist = input().split("|")
+result = []
+for index in range(len(sublist) - 1, -1, -1):
+    elements = sublist[index].split()
+    print(*elements, end=" ")
