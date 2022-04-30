bf3bc0c4d6e7a160973779cd1bbe5780c8cfc8da
BloodTaint <bloodtaint@yahoo.com>
2022-04-30 17:01:38 +0300

    30.04.22\1

---
+++ exams/python_advanced_retake_exam_14_april_2021/flights.py
@@ -0,0 +1,12 @@
+import sys
+from io import StringIO
+from pprint import pprint
+
+input1 = """"""
+
+input2 = """"""
+
+input3 = """"""
+
+sys.stdin = StringIO(input1)
+# sys.stdin = StringIO(input2)
