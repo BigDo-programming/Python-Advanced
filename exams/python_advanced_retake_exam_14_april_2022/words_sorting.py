d5dbcbfe3e713cfe426d49db62e45bcf9831b25d
Andon Andonov <bloodtaint@yahoo.com>
2022-05-03 18:08:50 +0300

    03.05.22\5

--- exams/python_advanced_retake_exam_14_april_2022/words_sorting.py
+++ exams/python_advanced_retake_exam_14_april_2022/words_sorting.py
@@ -1,21 +1,48 @@
-def words_sorting(param, param1, param2):
-    pass
+def words_sorting(*args):
+    sum_all_values = 0
+    sorted_dict = {}
+    for word in args:
+        symbol_sum = 0
+        for ch in word:
+            symbol_sum += ord(ch)
+        sorted_dict[word] = symbol_sum
+        sum_all_values += symbol_sum
 
+    if sum_all_values % 2 == 0:
+        sorted(sorted_dict.items(), key=lambda item: item[1])
+        data = ''
+        for key, value in sorted(sorted_dict.items()):
+            data += f"{key} - {value}" + '\n'
 
-print(
-    words_sorting(
-        'escape',
-        'charm',
-        'mythology'
-    ))
-print(
-    words_sorting(
-        'escape',
-        'charm',
-        'eye'
-    ))
-print(
-    words_sorting(
-        'cacophony',
-        'accolade'
-    ))
+        return data.strip()
+
+        # By keys in ascending order
+
+    else:
+        sorted(sorted_dict.items(), key=lambda item: item[1])
+        data = ''
+        for key, value in sorted_dict.items():
+            data += f"{key} - {value}" + '\n'
+
+        return data.strip()
+        # By values in descending order
+
+# ToDo 80/100
+#
+# print(
+#     words_sorting(
+#         'escape',
+#         'charm',
+#         'mythology'
+#     ))
+# print(
+#     words_sorting(
+#         'escape',
+#         'charm',
+#         'eye'
+#     ))
+# print(
+#     words_sorting(
+#         'cacophony',
+#         'accolade'
+#     ))
