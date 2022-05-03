ab1b8bc630ed27ee267ea62c41a98993d812ab46
BloodTaint <bloodtaint@yahoo.com>
2022-05-03 11:56:13 +0300

    03.05.22

--- exams/python_advanced_retake_exam_15_december_2021/christmas_elves.py
+++ exams/python_advanced_retake_exam_15_december_2021/christmas_elves.py
@@ -1,13 +1,84 @@
 import sys
 from io import StringIO
-from pprint import pprint
 
-input1 = """"""
+input1 = """10 16 13 25
+12 11 8"""
 
-input2 = """"""
+input2 = """10 14 22 4 5
+11 16 17 11 1 8"""
 
-input3 = """"""
+input3 = """5 6 7
+2 1 5 7 5 3"""
 
-# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input1)
 # sys.stdin = StringIO(input2)
-sys.stdin = StringIO(input3)
+# sys.stdin = StringIO(input3)
+
+from collections import deque
+
+elfs_energy = deque([int(x) for x in input().split()])
+materials_in_box = [int(x) for x in input().split()]
+
+total_elves_energy_used = 0
+total_successfully_made_toys = 0
+turn = 0
+
+while elfs_energy or materials_in_box:
+    if not elfs_energy:
+        break
+
+    if not materials_in_box:
+        break
+
+    elf = elfs_energy.popleft()
+    if elf < 5:
+        continue
+    turn += 1
+    box = materials_in_box.pop()
+
+    if turn % 3 == 0:
+        if not turn % 5 == 0:
+            if elf >= box * 2:
+                elf -= box * 2
+                total_successfully_made_toys += 2
+                total_elves_energy_used += box * 2
+                elf += 1
+                elfs_energy.append(elf)
+            else:
+                materials_in_box.append(box)
+                elfs_energy.append(elf * 2)
+
+        if turn % 5 == 0:
+            if elf >= box * 2:
+                elf -= box * 2
+                total_elves_energy_used += box * 2
+                elfs_energy.append(elf)
+
+            else:
+                materials_in_box.append(box)
+                elfs_energy.append(elf * 2)
+
+    elif turn % 5 == 0:
+        if elf >= box:
+            elf -= box
+            total_elves_energy_used += box
+            elfs_energy.append(elf)
+        else:
+            materials_in_box.append(box)
+            elfs_energy.append(elf * 2)
+
+    elif elf >= box:
+        elf -= box
+        total_successfully_made_toys += 1
+        total_elves_energy_used += box
+        elfs_energy.append(elf + 1)
+    else:
+        materials_in_box.append(box)
+        elfs_energy.append(elf * 2)
+
+print(f"Toys: {total_successfully_made_toys}")
+print(f"Energy: {total_elves_energy_used}")
+if elfs_energy:
+    print(f"Elves left: {', '.join([str(x) for x in elfs_energy])}")
+if materials_in_box:
+    print(f"Boxes left: {', '.join([str(x) for x in materials_in_box])}")
--- exams/python_advanced_retake_exam_15_december_2021/north_pole_challenge.py
+++ exams/python_advanced_retake_exam_15_december_2021/north_pole_challenge.py
@@ -2,12 +2,77 @@ import sys
 from io import StringIO
 from pprint import pprint
 
-input1 = """"""
+input1 = """6, 5
+. . . . .
+C . . G .
+. C . . .
+G . . C .
+. D . . D
+Y . . . G
+left-3
+up-1
+left-2
+right-7
+up-1
+End"""
 
-input2 = """"""
+input2 = """5, 6
+. . . . . .
+. . . . . .
+Y C D D . .
+. . . C . .
+. . . C . .
+right-3
+down-3"""
 
-input3 = """"""
+input3 = """5, 2
+. .
+. .
+. Y
+. .
+. G
+up-1
+left-11
+down-10
+End"""
 
-# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input1)
+
+
 # sys.stdin = StringIO(input2)
-sys.stdin = StringIO(input3)
+# sys.stdin = StringIO(input3)
+def move(direction, row, col):
+    if direction == "up":
+        return row - 1, col
+
+    if direction == "down":
+        return row + 1, col
+        pass
+
+    if direction == "left":
+        return row, col - 1
+        pass
+    if direction == "right":
+        return row, col + 1
+
+
+matrix = []
+my_row = 0
+my_col = 0
+n, m = [int(x) for x in input().split(', ')]
+for row in range(n):
+    value = input().split()
+    matrix.append(value)
+    for col in range(m):
+        if matrix[row][col] == "Y":
+            my_row, my_col = row, col
+pprint(matrix)
+command = input()
+while not command == "End":
+    direction, steps = command.split("-")
+    for i in range(int(steps)):
+        my_row, my_col = move(direction, my_row, my_col)
+        # prawim prowerka ako e izlqzal ot matricata da mine ot drugata strana i prawim prowerka kakwo ima w kletkata za da go wzemem i mestim sebesi i èrtaem pyt
+        print(my_row, my_col)
+
+    command = input()
