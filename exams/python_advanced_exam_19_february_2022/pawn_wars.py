8db847466736ecba679ae59944e21e9c9325445a
BloodTaint <bloodtaint@yahoo.com>
2022-05-05 11:58:49 +0300

    05.05.22\1

---
+++ exams/python_advanced_exam_19_february_2022/pawn_wars.py
@@ -0,0 +1,12 @@
+import sys
+from io import StringIO
+
+input1 = """"""
+
+input2 = """"""
+
+input3 = """"""
+
+# sys.stdin = StringIO(input1)
+# sys.stdin = StringIO(input2)
+sys.stdin = StringIO(input3)
---
+++ exams/python_advanced_exam_19_february_2022/springtime.py
@@ -0,0 +1,39 @@
+def start_spring(**kwargs):
+    my_dict = {}
+    for key, value in kwargs.items():
+        if value not in my_dict:
+            my_dict[value] = []
+        my_dict[value] += [key]
+    # my_dict_ = dict(sorted(my_dict.items(), key=lambda kv: (kv[0],kv[1])))
+    my_dict = dict(sorted(my_dict.items(), key=lambda kv: (-len(kv[1]), kv[0])))
+    # sorted(my_dict.items(),key=lambda x: len(x))
+    for key, value in my_dict.items():
+        print(key, sorted(value))
+
+
+# example_objects = {"Water Lilly": "flower",
+#                    "Swifts": "bird",
+#                    "Callery Pear": "tree",
+#                    "Swallows": "bird",
+#                    "Dahlia": "flower",
+#                    "Tulip": "flower", }
+#
+# print(start_spring(**example_objects))
+# example_objects = {"Swallow": "bird",
+#                    "Thrushes": "bird",
+#                    "Woodpeckers": "bird",
+#                    "Swallows": "bird",
+#                    "Warblers": "bird",
+#                    "Shrikes": "bird",}
+# print(start_spring(**example_objects))
+example_objects = {"Magnolia": "tree",
+                   "Swallow": "bird",
+                   "Thrushes": "bird",
+                   "Pear": "tree",
+                   "Cherries": "tree",
+                   "Shrikes": "bird",
+                   "Butterfly": "insect"}
+print(start_spring(**example_objects))
+
+# The collections sorted by their number of elements in descending order.
+    # o If two or more collections have the same number of elements in them, return them in ascending order (alphabetically) by the type's name.
