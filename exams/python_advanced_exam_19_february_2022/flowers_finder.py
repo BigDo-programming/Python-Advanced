816a7c1282dd9bb21a561f4bb36f5634a9d9c636
BloodTaint <bloodtaint@yahoo.com>
2022-05-05 14:29:37 +0300

    05.05.22\4

--- exams/python_advanced_exam_19_february_2022/flowers_finder.py
+++ exams/python_advanced_exam_19_february_2022/flowers_finder.py
@@ -1,12 +1,59 @@
 import sys
 from io import StringIO
 
-input1 = """"""
+input1 = """o e a o e a i
+p r s x r"""
 
-input2 = """"""
+input2 = """a a a
+x r l t p p"""
 
-input3 = """"""
+input3 = """u a o i u y o e
+p m t l"""
 
-# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input1)
 # sys.stdin = StringIO(input2)
-sys.stdin = StringIO(input3)
+# sys.stdin = StringIO(input3)
+from collections import deque
+
+
+def check(ch, flowers):
+    for i in range(len(flowers)):
+        for j in flowers[i]:
+            if ch == j:
+                flowers[i] = flowers[i].replace(ch, "*")
+
+    return flowers
+
+
+vowels = deque(input().split())
+consonants = input().split()
+
+finish = set()
+flowers = ["rose", "tulip", "lotus", "daffodil"]
+flowers_star = ["****", "*****", "*****", "********"]
+flowers_copy = ["rose", "tulip", "lotus", "daffodil"]
+
+while vowels or consonants:
+    if finish:
+        break
+    if not vowels:
+        break
+    if not consonants:
+        break
+    vowel = vowels.popleft()
+    flowers = check(vowel, flowers)
+    consonant = consonants.pop()
+    flowers = check(consonant, flowers)
+    for i in range(len(flowers)):
+        if flowers[i] == flowers_star[i]:
+            finish.add(flowers_copy[i])
+            break
+
+if finish:
+    print(f"Word found: {''.join(finish)}")
+else:
+    print(f"Cannot find any word!")
+if vowels:
+    print(f"Vowels left: {' '.join(vowels)}")
+if consonants:
+    print(f"Consonants left: {' '.join(consonants)}")
--- exams/python_advanced_exam_19_february_2022/pawn_wars.py
+++ exams/python_advanced_exam_19_february_2022/pawn_wars.py
@@ -1,12 +1,58 @@
 import sys
 from io import StringIO
 
-input1 = """"""
+input1 = """- - - - - - b -
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+- w - - - - - -
+- - - - - - - -
+- - - - - - - -"""
 
-input2 = """"""
+input2 = """- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+b - - - - - - -
+- w - - - - - -
+- - - - - - - -"""
 
 input3 = """"""
 
-# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input1)
 # sys.stdin = StringIO(input2)
-sys.stdin = StringIO(input3)
+# sys.stdin = StringIO(input3)
+
+size = 8
+matrix = []
+black_row = 0
+black_col = 0
+white_row = 0
+white_col = 0
+
+for row in range(size):
+    value = input().split()
+    matrix.append(value)
+    for col in range(size):
+        if matrix[row][col] == "b":
+            black_row, black_col = row, col
+        if matrix[row][col] == "w":
+            white_row, white_col = row, col
+if abs(white_col - black_col) == 1:
+    # captures()
+    print("fight")
+else:
+
+    white = white_row - 0
+    black = abs(black_row - 8)
+    if black > white:
+        print("white")
+    else:
+        print("black")
+#     print(white)
+#     print(black)
+# #     # go_for_queen()
+# print(white_row)
+# print(black_row)
