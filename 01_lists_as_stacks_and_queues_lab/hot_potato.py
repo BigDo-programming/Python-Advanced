048fed22a9f20cc7921236c80b4691f0a11e193f
bloodtaint <bloodtaint@yahoo.com>
2022-05-17 07:11:18 +0300

    17.05.22

--- 01_lists_as_stacks_and_queues_lab/hot_potato.py
+++ 01_lists_as_stacks_and_queues_lab/hot_potato.py
@@ -11,12 +11,26 @@ from collections import deque
 potato_players = deque(input().split())
 n = int(input())
 
-while potato_players:
-    for _ in range(n - 1):
-        potato_players.append(potato_players.popleft())
-    out_player = potato_players.popleft()
-    if potato_players:
-        print(f'Removed {out_player}')
-    else:
-        print(f'Last is {out_player}')
+# while potato_players:
+#     for _ in range(n - 1):
+#         potato_players.append(potato_players.popleft())
+#     out_player = potato_players.popleft()
+#     if potato_players:
+#         print(f'Removed {out_player}')
+#     else:
+#         print(f'Last is {out_player}')
 
+current_count = 0
+while len(potato_players) > 1:
+    
+    current_count += 1
+    kid = potato_players.popleft()
+    
+    if current_count < n:
+        potato_players.append(kid)
+        
+    else:
+        print(f'Removed {kid}')
+        current_count = 0
+
+print(f'Last is {potato_players.popleft()}')
---
+++ other/test.py
@@ -0,0 +1,6 @@
+from collections import deque
+
+q = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(q)
+q.rotate(4)
+print(q)
