2950b698a780fbda5db6beb97786974d7343a2b3
BloodTaint <bloodtaint@yahoo.com>
2022-05-20 18:15:23 +0300

    20.05.22.3

--- 03_tuples_and_sets_lab/count_same_values.py
+++ 03_tuples_and_sets_lab/count_same_values.py
@@ -3,25 +3,47 @@ import sys
 from io import StringIO
 
 input1 = "-2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3"
+input2 = "2 4 4 5 5 2 3 3 4 4 3 3 4 3 5 3 2 5 4 3"
 
-sys.stdin = StringIO(input1)
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
 
-same_values = {}
-values = [float(x) for x in input().split()]
-for i in values:
-    if i not in same_values:
-        same_values[i] = 1
-    else:
-        same_values[i] += 1
-
-
-for key,value in same_values.items():
+values = {}
+text_to_numbers = [float(x) for x in input().split()]
+for i in text_to_numbers:
+    if i not in values:
+        values[i] = 0
+    values[i] += 1
+for key,value in values.items():
     print(f"{key} - {value} times")
 
 
 
 
 
+
+
+
+
+
+
+
+# same_values = {}
+# values = [float(x) for x in input().split()]
+# for i in values:
+#     if i not in same_values:
+#         same_values[i] = 1
+#     else:
+#         same_values[i] += 1
+#
+#
+# for key,value in same_values.items():
+#     print(f"{key} - {value} times")
+#
+#
+#
+#
+#
 
 
 
--- 03_tuples_and_sets_lab/students'_grades.py
+++ 03_tuples_and_sets_lab/students'_grades.py
@@ -13,15 +13,31 @@ Alex 3.00"""
 sys.stdin = StringIO(input1)
 
 students_grades = {}
+
 n = int(input())
-for i in range(n):
+for _ in range(n):
     name, grade = input().split()
     if name not in students_grades:
         students_grades[name] = []
-    students_grades[name].append(float(grade))
+    students_grades[name] += [float(grade)]
+
 
 for name, grade in students_grades.items():
     print(f"{name} -> {' '.join(str(f'{x:.2f}') for x in grade)} (avg: {sum(grade) / len(grade):.02f})")
+
+
+
+
+# students_grades = {}
+# n = int(input())
+# for i in range(n):
+#     name, grade = input().split()
+#     if name not in students_grades:
+#         students_grades[name] = []
+#     students_grades[name].append(float(grade))
+#
+# for name, grade in students_grades.items():
+#     print(f"{name} -> {' '.join(str(f'{x:.2f}') for x in grade)} (avg: {sum(grade) / len(grade):.02f})")
 
 # def avg(values):
 #     return sum(values) / len(values)
