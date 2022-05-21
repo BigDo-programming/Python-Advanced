86dafa2af79fc0f7610fa6891b15638f569218ae
bloodtaint <bloodtaint@yahoo.com>
2022-05-21 07:51:15 +0300

    21.05.22

--- 02_lists_as_stacks_and_queues_exercise/robotics.py
+++ 02_lists_as_stacks_and_queues_exercise/robotics.py
@@ -20,64 +20,69 @@ End"""
 # sys.stdin = StringIO(input1)
 sys.stdin = StringIO(input2)
 from math import floor
+
+
 from collections import deque
 
 
-def robots_and_time():
-    robots_time = deque()
-    bots = input().split(";")
-    for i in bots:
-        robot_, time_ = i.split("-")
-        robots_time.append([robot_, int(time_), 0])
-    return robots_time
+def read_robots():
+    result = {}
+    robots_input = input().split(";")
+    for i in robots_input:
+        robots_details = i.split("-")
+        name = robots_details[0]
+        processing_time = int(robots_details[1])
+        result[name] = processing_time
+    return result
 
 
-# processing_time
+def to_seconds(hours, minutes, seconds):
+    return hours * 60 * 60 + minutes * 60 + seconds
 
 
-robots = robots_and_time()
-product_line = deque()
-time_start = input()  # da go oprawq
-time_start = 0
-
-command = input()
-while not command == "End":
-    product_line.append(command)
-    command = input()
-
-working = {}
-while product_line:
-    product = product_line.popleft()
-    time_start +=1
-    for robot, time, work in robots:
-        if work == 0:
-            working[time_start + 1] = [robot, product, time + time_start]
-            robots[0][2] = 1
-
-            robots.rotate(-1)
-
-        elif work == 1:
-            for name, time_end, work_mode in working.items():
-                if time_end == time_start:
-                    j[2] = 0
-            #         da go nawyrva s spisakyt na robota!!!
-
-            else:
-                # time_start += 1
-                product_line.appendleft(product)
-
-print(working)
+def to_str_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 3600) % 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 
+def read_products():
+    result = deque()
+    while True:
+        line = input()
+        if line == "End":
+            break
+        result.append(line)
+    return result
 
 
+robots = read_robots()
+available_robots = [k for k in robots]
+processing_robots = {}
 
+starting_time_parts = [int(x) for x in input().split(":")]
+time_in_seconds = to_seconds(starting_time_parts[0], starting_time_parts[1], starting_time_parts[2])
 
+products = read_products()
 
+while products:
+    time_in_seconds = (time_in_seconds + 1) % (24 * 60 * 60)
 
+    for robot_name in [k for k in processing_robots]:
+        processing_robots[robot_name] -= 1
+        if processing_robots[robot_name] <= 0:
+            processing_robots.pop(robot_name)
 
+    current_product = products.popleft()
+    for robot_name in available_robots:
+        if robot_name not in processing_robots:
+            print(f"{robot_name} - {current_product} [{to_str_time(time_in_seconds)}]")
+            processing_robots[robot_name] = robots[robot_name]
+            break
+    else:
+        products.append(current_product)
 
-# ToDo да я довърша
 # class Robot:
 #     def __init__(self, name, processing_time):
 #         self.processing_time = processing_time
@@ -127,3 +132,48 @@ print(working)
 #             break
 #     if not found_robot:
 #         items.append(current_item)
+
+
+#
+# def robots_and_time():
+#     robots_time = deque()
+#     bots = input().split(";")
+#     for i in bots:
+#         robot_, time_ = i.split("-")
+#         robots_time.append([robot_, int(time_), 0])
+#     return robots_time
+
+
+# robots = robots_and_time()
+# product_line = deque()
+# time_start = input()  # da go oprawq
+# time_start = 0
+#
+# command = input()
+# while not command == "End":
+#     product_line.append(command)
+#     command = input()
+#
+# working = {}
+# while product_line:
+#     product = product_line.popleft()
+#     time_start +=1
+#     for robot, time, work in robots:
+#         if work == 0:
+#             working[time_start + 1] = [robot, product, time + time_start]
+#             robots[0][2] = 1
+#
+#             robots.rotate(-1)
+#
+#         elif work == 1:
+#             for name, time_end, work_mode in working.items():
+#                 if time_end == time_start:
+#                     j[2] = 0
+#             #         da go nawyrva s spisakyt na robota!!!
+#
+#             else:
+#                 # time_start += 1
+#                 product_line.appendleft(product)
+#
+# print(working)
+# # ToDo да я довърша
