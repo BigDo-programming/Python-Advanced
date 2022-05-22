5828de39456f0547e359f9a224dbb997839e2892
BloodTaint <bloodtaint@yahoo.com>
2022-05-22 21:36:50 +0300

    22.05.22\4

--- 04_tuples_and_sets_exercise/battle_of_names.py
+++ 04_tuples_and_sets_exercise/battle_of_names.py
@@ -16,30 +16,53 @@ Pesho
 Stefan"""
 
 sys.stdin = StringIO(input1)
+
 # sys.stdin = StringIO(input2)
 
-odd_set = set()
-even_set = set()
 n = int(input())
-
+even_set = set()
+odd_set = set()
 for i in range(1, n + 1):
-    name_sum = 0
     name = input()
+
+    name_sum = 0
     for ch in name:
         name_sum += ord(ch)
-    if name_sum // i % 2 == 0:
-        even_set.add(name_sum // i)
+    ch_number = name_sum // i
+    if ch_number % 2 == 0:
+        even_set.add(ch_number)
     else:
-        odd_set.add(name_sum // i)
+        odd_set.add(ch_number)
 
-if sum(odd_set) == sum(even_set):
+if sum(even_set) == sum(odd_set):
+    print(*odd_set.union(even_set), sep=", ")
+elif sum(even_set) < sum(odd_set):
+    print(*odd_set.difference(even_set), sep=", ")
+elif sum(even_set) > sum(odd_set):
+    print(*odd_set.symmetric_difference(even_set), sep=", ")
 
-    print(", ".join([str(x) for x in odd_set.union(even_set)]))
-elif sum(odd_set) > sum(even_set):
-
-    print(", ".join([str(x) for x in odd_set.difference(even_set)]))
-else:
-    print(", ".join([str(x) for x in odd_set.symmetric_difference(even_set)]))
+# odd_set = set()
+# even_set = set()
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     name_sum = 0
+#     name = input()
+#     for ch in name:
+#         name_sum += ord(ch)
+#     if name_sum // i % 2 == 0:
+#         even_set.add(name_sum // i)
+#     else:
+#         odd_set.add(name_sum // i)
+#
+# if sum(odd_set) == sum(even_set):
+#
+#     print(", ".join([str(x) for x in odd_set.union(even_set)]))
+# elif sum(odd_set) > sum(even_set):
+#
+#     print(", ".join([str(x) for x in odd_set.difference(even_set)]))
+# else:
+#     print(", ".join([str(x) for x in odd_set.symmetric_difference(even_set)]))
 
 
 # n = int(input())
@@ -48,11 +71,6 @@ else:
 # for i in range(1, n + 1):
 #     name = input()
 #     name_sum = sum([ord(x) for x in name]) // i
-#
-#     #     sum_ascii_value = 0
-#     #     for ch in name:
-#     #         sum_ascii_value += ord(ch)
-#     #     number = sum_ascii_value // i
 #
 #     if name_sum % 2 == 0:
 #         even_set.add(name_sum)
