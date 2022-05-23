aac31f3724f422dd1436adf049e36f087a3a02b1
BloodTaint <bloodtaint@yahoo.com>
2022-05-23 17:01:14 +0300

    23.05.22\4

--- 05_stacks_queues_tuples_and_sets_exercise/honey.py
+++ 05_stacks_queues_tuples_and_sets_exercise/honey.py
@@ -9,40 +9,101 @@ input2 = """30
 15 9 5 150 8
 * + + * -"""
 
-input3 = """"""
+input3 = """0
+0 9 5 150 0
+/ + + * -"""
 
 # sys.stdin = StringIO(input1)
 sys.stdin = StringIO(input2)
 # sys.stdin = StringIO(input3)
 from collections import deque
 
-symbols = {
-    "+": lambda a, b: a + b,
-    "-": lambda a, b: a - b,
-    "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b if b > 0 else 0,
-}
+honey = 0
 working_bees = deque([int(x) for x in input().split()])
 nectar = [int(x) for x in input().split()]
-honey_making_process = deque(input().split())
-total_honey = 0
+symbols = deque(input().split())
 
-while working_bees and nectar:
+while True:
+    if not working_bees:
+        break
+    if not nectar:
+        break
 
     bee = working_bees.popleft()
-    nectar_current = nectar.pop()
+    nectar_pop = nectar.pop()
 
-    if nectar_current >= bee:
-
-        total_honey += abs(symbols[honey_making_process.popleft()](bee, nectar_current))
-    else:
+    if nectar_pop < bee:
         working_bees.appendleft(bee)
+    else:
+        symbol = symbols.popleft()
+        if symbol == "-":
+            honey += abs(bee - nectar_pop)
+        elif symbol == "+":
+            honey += abs(bee + nectar_pop)
+        elif symbol == "*":
+            honey += abs(bee * nectar_pop)
+        elif symbol == "/":
+            if not bee == 0 and not nectar_pop == 0:
+                honey += abs(bee / nectar_pop)
 
-print(f"Total honey made: {total_honey}")
+
+print(f"Total honey made: {honey}")
 if working_bees:
     print(f"Bees left: {', '.join([str(x) for x in working_bees])}")
 if nectar:
     print(f"Nectar left: {', '.join([str(x) for x in nectar])}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# symbols = {
+#     "+": lambda a, b: a + b,
+#     "-": lambda a, b: a - b,
+#     "*": lambda a, b: a * b,
+#     "/": lambda a, b: a / b if b > 0 else 0,
+# }
+# working_bees = deque([int(x) for x in input().split()])
+# nectar = [int(x) for x in input().split()]
+# honey_making_process = deque(input().split())
+# total_honey = 0
+#
+# while working_bees and nectar:
+#
+#     bee = working_bees.popleft()
+#     nectar_current = nectar.pop()
+#
+#     if nectar_current >= bee:
+#
+#         total_honey += abs(symbols[honey_making_process.popleft()](bee, nectar_current))
+#     else:
+#         working_bees.appendleft(bee)
+#
+# print(f"Total honey made: {total_honey}")
+# if working_bees:
+#     print(f"Bees left: {', '.join([str(x) for x in working_bees])}")
+# if nectar:
+#     print(f"Nectar left: {', '.join([str(x) for x in nectar])}")
 
 
 
