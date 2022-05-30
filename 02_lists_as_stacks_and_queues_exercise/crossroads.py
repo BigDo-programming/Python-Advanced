6d91c747d354d8013eb9277aa26212742e8ae2cd
bloodtaint <bloodtaint@yahoo.com>
2022-05-30 21:48:16 +0300

    30.05.22.4

--- 02_lists_as_stacks_and_queues_exercise/crossroads.py
+++ 02_lists_as_stacks_and_queues_exercise/crossroads.py
@@ -21,38 +21,87 @@ Mercedes
 green
 END"""
 
+input3 = """10
+5
+Mercedes
+green
+Mercedes
+Mercedes
+Skoda
+green
+END"""
+
 # sys.stdin = StringIO(input1)
 sys.stdin = StringIO(input2)
+# sys.stdin = StringIO(input3)
 
 from collections import deque
 
-cars = deque()
 green_light = int(input())
 free_window = int(input())
-
 passed_cars = 0
-crash_happened = False
-
-while not crash_happened:
-    line = input()
-    if line == "END":
+crossroad = deque()
+while True:
+    data = input()
+    if data == "END":
         break
-    if line == "green":
-        current_green_light = green_light
-        while cars and current_green_light > 0:
-            car = cars.popleft()
-            if current_green_light >= len(car) or current_green_light + free_window >= len(car):
-                passed_cars += 1
-                current_green_light -= len(car)
-            else:
-                print("A crash happened!")
-                print(f"{car} was hit at {car[current_green_light+free_window]}.")
-                crash_happened = True
-                break
-    else:
-        cars.append(line)
-if not crash_happened:
-    print("Everyone is safe.")
-    print(f"{passed_cars} total cars passed the crossroads.")
 
-# ToDo - da q resha sam
+    if not data == "green":
+        crossroad.append(data)
+
+    else:
+        max_sec = green_light + free_window
+        while crossroad:
+
+            car = crossroad.popleft()
+            if len(car) <= max_sec:
+                max_sec -= len(car)
+                passed_cars += 1
+
+            elif max_sec <= free_window:
+                continue
+
+            elif len(car) > free_window:
+                hit = len(car) - max_sec
+                print("A crash happened!")
+                print(f"{car} was hit at {car[-hit]}.")
+                exit()
+
+
+print("Everyone is safe.")
+print(f"{passed_cars} total cars passed the crossroads.")
+
+# ToDO 85 / 100
+
+
+# from collections import deque
+#
+# cars = deque()
+# green_light = int(input())
+# free_window = int(input())
+#
+# passed_cars = 0
+# crash_happened = False
+#
+# while not crash_happened:
+#     line = input()
+#     if line == "END":
+#         break
+#     if line == "green":
+#         current_green_light = green_light
+#         while cars and current_green_light > 0:
+#             car = cars.popleft()
+#             if current_green_light >= len(car) or current_green_light + free_window >= len(car):
+#                 passed_cars += 1
+#                 current_green_light -= len(car)
+#             else:
+#                 print("A crash happened!")
+#                 print(f"{car} was hit at {car[current_green_light+free_window]}.")
+#                 crash_happened = True
+#                 break
+#     else:
+#         cars.append(line)
+# if not crash_happened:
+#     print("Everyone is safe.")
+#     print(f"{passed_cars} total cars passed the crossroads.")
+#
--- 05_stacks_queues_tuples_and_sets_exercise/paint_colors.py
+++ 05_stacks_queues_tuples_and_sets_exercise/paint_colors.py
@@ -1,4 +1,5 @@
 import sys
+
 from io import StringIO
 
 input1 = """d yel blu e low redd"""
@@ -13,122 +14,98 @@ sys.stdin = StringIO(input3)
 
 from collections import deque
 
-needed_colors = {
-    "orange": ["red", "yellow"],
-    "purple": ["red", "blue"],
-    "green": ["yellow", "blue"]
+text = deque(input().split())
+
+main_colors = {"red", "yellow", "blue"}
+secondary_colors = {"orange", "purple", "green"}
+collected_colors = []
+while text:
+    first_word = text.popleft()
+    last_word = text.pop() if text else ""
+    word = first_word + last_word
+
+    if word in main_colors or word in secondary_colors:
+        collected_colors.append(word)
+        continue
+
+    word = last_word + first_word
+    if word in main_colors or word in secondary_colors:
+        collected_colors.append(word)
+        continue
+
+    first_word = first_word[:-1]
+    last_word = last_word[:-1]
+    if first_word:
+        text.insert(len(text) // 2, first_word)
+    if last_word:
+        text.insert(len(text) // 2, last_word)
+
+result = []
+required_colors = {
+    'orange': ['red', 'yellow'],
+    'purple': ['red', 'blue'],
+    'green': ['yellow', 'blue'],
 }
-
-colors = ["red", "yellow", "blue", "orange", "purple", "green"]
-
-main = ["red", "yellow", "blue"]
-main_colors = []
-secondary = ["orange", "purple", "green"]
-secondary_colors = []
-original_list = []
-
-string = deque(input().split())
-while string:
-
-    first = string.popleft()
-    last = string.pop() if string else ""
-
-    if first + last in colors:
-        original_list.append(first + last)
-
-        if first + last in main:
-            main_colors.append(first + last)
-
-        else:
-            secondary_colors.append(first + last)
-
-    elif last + first in colors:
-        original_list.append(last + first)
-
-        if last + first in main:
-            main_colors.append(last + first)
-        else:
-            secondary_colors.append(last + first)
-
+for color in collected_colors:
+    if color in main_colors:
+        result.append(color)
     else:
-        middle = len(string) // 2
-        if len(string) == 0:
-            break
-
-        string.insert(middle, first[:-1] + last[: -1])
-
-
-    # elif len(string) == 1:
-    #     first = string.pop()
-    #
-    #     if first in colors:
-    #         original_list.append(first)
-    #
-    #         if first in main:
-    #             main_colors.append(first)
-    #         else:
-    #             secondary_colors.append(first)
-
-for i in secondary_colors:
-    one, two = [x for x in needed_colors[i]]
-    if one not in main_colors or two not in main_colors or one not in main_colors and two not in main_colors:
-        original_list.remove(i)
-
-print(original_list)
-
-# ToDo 60/100
+        is_valid = True
+        for required_color in required_colors[color]:
+            if required_color not in collected_colors:
+                is_valid = False
+                break
+        if is_valid:
+            result.append(color)
+print(result)
 
 
-
-
-
-
-
-
-
-
-
-# text = deque(input().split())
+# from collections import deque
 #
-# main_colors = {"red", "yellow", "blue"}
-# secondary_colors = {"orange", "purple", "green"}
-# collected_colors = []
-# while text:
-#     first_word = text.popleft()
-#     last_word = text.pop() if text else ""
-#     word = first_word + last_word
+# needed_colors = {
+#     "orange": ["red", "yellow"],
+#     "purple": ["red", "blue"],
+#     "green": ["yellow", "blue"]
+# }
 #
-#     if word in main_colors or word in secondary_colors:
-#         collected_colors.append(word)
+# main = ["red", "yellow", "blue"]
+# secondary = ["orange", "purple", "green"]
+#
+#
+# original_list = []
+#
+# string = deque(input().split())
+# while string:
+#
+#     first = string.popleft()
+#     last = string.pop() if string else ""
+#
+#     if first + last in main or first + last in secondary:
+#         original_list.append(first + last)
 #         continue
 #
-#     word = last_word + first_word
-#     if word in main_colors or word in secondary_colors:
-#         collected_colors.append(word)
+#     if last + first in main or last + first in secondary:
+#         original_list.append(last + first)
 #         continue
 #
-#     first_word = first_word[:-1]
-#     last_word = last_word[:-1]
-#     if first_word:
-#         text.insert(len(text) // 2, first_word)
-#     if last_word:
-#         text.insert(len(text) // 2, last_word)
+#     first = first[:-1]
+#     last = last[:-1]
+#
+#     if first:
+#         string.insert(len(string)//2, first)
+#     if last:
+#         string.insert(len(string)//2, last)
+#
 #
 # result = []
-# required_colors = {
-#     'orange': ['red', 'yellow'],
-#     'purple': ['red' , 'blue'],
-#     'green': ['yellow', 'blue'],
-# }
-# for color in collected_colors:
-#     if color in main_colors:
+# for color in original_list:
+#     if color not in secondary:
 #         result.append(color)
 #     else:
-#         is_valid = True
-#         for required_color in required_colors[color]:
-#             if required_color not in collected_colors:
-#                 is_valid = False
-#                 break
-#         if is_valid:
+#
+#         color1,color2 = [x for x in needed_colors[color]]
+#         if color1 in original_list and color2 in original_list:
 #             result.append(color)
+#
+#
 # print(result)
