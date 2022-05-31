ff435b57bf2644adb8d6ab553dba0c7b06694ef9
bloodtaint <bloodtaint@yahoo.com>
2022-05-31 20:57:46 +0300

    31.05.22.4

--- 10_functions_advanced_exercise/negative_vs_positive.py
+++ 10_functions_advanced_exercise/negative_vs_positive.py
@@ -4,18 +4,41 @@ from io import StringIO
 input1 = """1 2 -3 -4 65 -98 12 57 -84"""
 
 sys.stdin = StringIO(input1)
-my_numbers = [int(x) for x in input().split()]
-
-negative = [x for x in my_numbers if x < 0]
-positive = [x for x in my_numbers if x > 0]
 
 
-negative_sum = sum(negative)
-positive_sum = sum(positive)
+def stronger_positive_or_negative(*args):
+    positive = [x for x in args if x > 0]
+    negative = [x for x in args if x <= 0]
+    print(sum(negative))
+    print(sum(positive))
+    # positive_or_negative = "positives" if sum(positive) > sum(negative) else "negatives"
+    if abs(sum(negative)) > sum(positive):
+        print("The negatives are stronger than the positives")
+    else:
+        print("The positives are stronger than the negatives")
 
-print(negative_sum)
-print(positive_sum)
-if abs(negative_sum) > positive_sum:
-    print("The negatives are stronger than the positives")
-else:
-    print("The positives are stronger than the negatives")
+
+
+nums = [int(x) for x in input().split()]
+stronger_positive_or_negative(*nums)
+
+
+
+
+
+
+# my_numbers = [int(x) for x in input().split()]
+#
+# negative = [x for x in my_numbers if x < 0]
+# positive = [x for x in my_numbers if x > 0]
+#
+#
+# negative_sum = sum(negative)
+# positive_sum = sum(positive)
+#
+# print(negative_sum)
+# print(positive_sum)
+# if abs(negative_sum) > positive_sum:
+#     print("The negatives are stronger than the positives")
+# else:
+#     print("The positives are stronger than the negatives")
