b23d7ce61c0284c0f902aff93c840d37992ca413
BloodTaint <bloodtaint@yahoo.com>
2022-06-01 18:01:18 +0300

    1.06.22.1

--- 10_functions_advanced_exercise/recursion_palindrome.py
+++ 10_functions_advanced_exercise/recursion_palindrome.py
@@ -1,14 +1,29 @@
-def palindrome(text, idx):
-    if idx == len(text) // 2:
-        return f"{text} is a palindrome"
-    left = text[idx]
-    right = text[len(text) - 1 - idx]
+def palindrome(word, index):
+    if index >= len(word) // 2:
+        return f"{word} is a palindrome"
 
-    if left != right:
-        return f"{text} is not a palindrome"
+    word2 = word[::-1]
+    if word[index] == word2[index]:
 
-    return palindrome(text, idx + 1)
+        return palindrome(word, index + 1)
 
+    else:
+        return f"{word} is not a palindrome"
 
 print(palindrome("abcba", 0))
-# print(palindrome("peter", 0))
+print(palindrome("peter", 0))
+
+
+
+
+
+# def palindrome(text, idx):
+#     if idx == len(text) // 2:
+#         return f"{text} is a palindrome"
+#     left = text[idx]
+#     right = text[len(text) - 1 - idx]
+#
+#     if left != right:
+#         return f"{text} is not a palindrome"
+#
+#     return palindrome(text, idx + 1)
