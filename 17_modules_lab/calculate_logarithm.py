1ceb7ccb35d142a466b80de97d28cfbc56dde857
bloodtaint <bloodtaint@yahoo.com>
2022-06-05 22:11:16 +0300

    05.06.22

--- 17_modules_lab/calculate_logarithm.py
+++ 17_modules_lab/calculate_logarithm.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from io import StringIO
 
@@ -6,3 +7,15 @@ input1 = """"""
 input2 = """"""
 
 sys.stdin = StringIO(input1)
+
+
+def calculate_log(x, base_as_str):
+    if base_as_str == "natural":
+        return math.log(x)
+
+    base = int(base_as_str)
+    return math.log(x,base)
+
+
+print(calculate_log(10,10))
+print(calculate_log(10,'natural'))
