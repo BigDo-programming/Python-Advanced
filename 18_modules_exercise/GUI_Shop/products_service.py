56e20338e17cdf57ca07e2b801e73b56224abcbf
BloodTaint <bloodtaint@yahoo.com>
2022-06-06 17:48:05 +0300

    06.06.22.4

--- 18_modules_exercise/GUI_Shop/products_service.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def get_all_products():
-    products = []
-    with open("../db/products.txt", "r") as file:
-        for line in file:
-            products.append(json.loads(line.strip()))
-        return products
