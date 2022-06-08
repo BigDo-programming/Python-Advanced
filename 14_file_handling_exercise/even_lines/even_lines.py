2de9f24516ffe68b43ac8f9fbe5ddf035930ca77
BloodTaint <bloodtaint@yahoo.com>
2022-06-08 14:55:00 +0300

    07.06.22.

--- 14_file_handling_exercise/even_lines/even_lines.py
+++ 14_file_handling_exercise/even_lines/even_lines.py
@@ -1,6 +1,31 @@
 # Write a program that reads a text file and prints on the console its even lines.
 # Line numbers start from 0. Before you print the result, replace  with "@" and reverse the order of the words.
 
+# def replace_ch(text):
+#     symbols = ["-", ",", ".", "!", "?"]
+#
+#     for symbol in symbols:
+#         text = text.replace(symbol, "@")
+#
+#     return text.strip()
+#
+#
+# with open("text.txt") as file:
+#     index = 0
+#     while True:
+#         line = file.readline()
+#
+#         if not line:
+#             break
+#
+#         if index % 2 == 0:
+#             line = (replace_ch(line))
+#             print(" ".join(line.split()[::-1]))
+#
+#         index += 1
+
+
+
 def replace_ch(text):
     symbols = ["-", ",", ".", "!", "?"]
 
@@ -9,17 +34,9 @@ def replace_ch(text):
 
     return text.strip()
 
-
 with open("text.txt") as file:
-    index = 0
-    while True:
-        line = file.readline()
 
-        if not line:
-            break
-
-        if index % 2 == 0:
-            line = (replace_ch(line))
-            print(" ".join(line.split()[::-1]))
-
-        index += 1
+    for i, char in enumerate(file):
+        if i % 2 == 0:
+            string = replace_ch(char)
+            print(' '.join(string.split()[::-1]))
