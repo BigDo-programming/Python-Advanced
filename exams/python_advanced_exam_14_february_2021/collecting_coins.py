789beadd84dda8d6846f453d633f6c2e6449fcbc
BloodTaint <bloodtaint@yahoo.com>
2022-06-21 22:38:17 +0300

    21.06.22

--- exams/python_advanced_exam_14_february_2021/collecting_coins.py
+++ exams/python_advanced_exam_14_february_2021/collecting_coins.py
@@ -33,59 +33,110 @@ input3 = """"""
 
 sys.stdin = StringIO(input1)
 # sys.stdin = StringIO(input2)
-
-
 # sys.stdin = StringIO(input3)
-def move(direction, row, col):
-    if direction == "up":
+
+
+def move(row, col, direction):
+    if direction == 'up':
         return row - 1, col
-    if direction == "down":
+    if direction == 'down':
         return row + 1, col
-    if direction == "left":
+    if direction == 'left':
         return row, col - 1
-    if direction == "right":
+    if direction == 'right':
         return row, col + 1
 
 
 path = []
 coins = 0
 size = int(input())
-player_row = 0
-player_col = 0
-
 matrix = []
+player_row, player_col = 0, 0
 for r in range(size):
-    value = input().split()
+    value = [int(x) if x.isdigit() else str(x) for x in input().split()]
     matrix.append(value)
     for c in range(size):
-        if matrix[r][c] == "P":
+        if matrix[r][c] == 'P':
             player_row, player_col = r, c
-path.append([player_row, player_col])
-while True:
-    command = input()
-    player_row, player_col = move(command, player_row, player_col)
-    if 0 > player_row:
-        player_row = player_row % size
-    if 0 > player_col:
-        player_col = player_col % size
-    if player_row >= size:
-        player_row = player_row % size
-    if player_col >= size:
-        player_col = player_col % size
-    path.append([player_row, player_col])
-    if matrix[player_row][player_col].isdigit():
-        coins += int(matrix[player_row][player_col])
-        matrix[player_row][player_col] = "*"
-    if matrix[player_row][player_col] == "X":
-        coins //= 2
-        break
-    if coins >= 100:
-        break
 
+matrix[player_row][player_col] = 0
+path.append(f'[{player_row}, {player_col}]')
+
+
+while coins < 100:
+    command = input()
+
+    player_row, player_col = move(player_row, player_col, command)
+    player_row %= size
+    player_col %= size
+
+    path.append(f'[{player_row}, {player_col}]')
+    if matrix[player_row][player_col] == 'X':
+        coins *= 0.5
+
+        break
+    coins += matrix[player_row][player_col]
+    matrix[player_row][player_col] = 0
 
 if coins >= 100:
     print(f"You won! You've collected {coins} coins.")
 else:
-    print(f"Game over! You've collected {coins} coins.")
-print(f"Your path:")
-[print(x)for x in path]
+    print(f"Game over! You've collected {int(coins)} coins.")
+print('Your path:')
+[print(x) for x in path]
+
+
+
+# def move(direction, row, col):
+#     if direction == "up":
+#         return row - 1, col
+#     if direction == "down":
+#         return row + 1, col
+#     if direction == "left":
+#         return row, col - 1
+#     if direction == "right":
+#         return row, col + 1
+#
+#
+# path = []
+# coins = 0
+# size = int(input())
+# player_row = 0
+# player_col = 0
+#
+# matrix = []
+# for r in range(size):
+#     value = input().split()
+#     matrix.append(value)
+#     for c in range(size):
+#         if matrix[r][c] == "P":
+#             player_row, player_col = r, c
+# path.append([player_row, player_col])
+# while True:
+#     command = input()
+#     player_row, player_col = move(command, player_row, player_col)
+#     if 0 > player_row:
+#         player_row = player_row % size
+#     if 0 > player_col:
+#         player_col = player_col % size
+#     if player_row >= size:
+#         player_row = player_row % size
+#     if player_col >= size:
+#         player_col = player_col % size
+#     path.append([player_row, player_col])
+#     if matrix[player_row][player_col].isdigit():
+#         coins += int(matrix[player_row][player_col])
+#         matrix[player_row][player_col] = "*"
+#     if matrix[player_row][player_col] == "X":
+#         coins //= 2
+#         break
+#     if coins >= 100:
+#         break
+#
+#
+# if coins >= 100:
+#     print(f"You won! You've collected {coins} coins.")
+# else:
+#     print(f"Game over! You've collected {coins} coins.")
+# print(f"Your path:")
+# [print(x)for x in path]
