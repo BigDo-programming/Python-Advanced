0346989508488abd69bac08b9146d447215e6501
BloodTaint <bloodtaint@yahoo.com>
2022-06-25 10:16:57 +0300

    25.06.22

--- exams/python_advanced_retake_exam_16_december_2020/magic_triangle.py
+++ exams/python_advanced_retake_exam_16_december_2020/magic_triangle.py
@@ -1,15 +1,19 @@
 def get_magic_triangle(n):
     triangle = [[1], [1, 1]]
-    for row_index in range(2, n):
+
+    for row in range(2, n):
         triangle.append([])
+
         triangle[-1].append(1)
-        for col_index in range(1, row_index):
-            left = triangle[row_index - 1][col_index - 1]
-            right = triangle[row_index - 1][col_index]
+        for col in range(1, row):
+            left = triangle[row - 1][col - 1]
+            right = triangle[row - 1][col]
             triangle[-1].append(left + right)
 
         triangle[-1].append(1)
+
     return triangle
+    # [print(x) for x in triangle]
 
 
-get_magic_triangle(5)
+print(get_magic_triangle(5))
